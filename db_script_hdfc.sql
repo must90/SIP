a8381a1b8176b6e65d65aea2d6ad88b6f47cd32b
must90 <must90@gmail.com>
2020-09-03 15:07:12 +0530

    Add files via upload

---
+++ db_script_hdfc.sql
@@ -0,0 +1,11 @@
+create table tbl_hdfc_pf (
+hdfc_id serial,
+hdfc_fund_name character varying,
+isin character varying,
+coupon character varying,
+instrument_name character varying,
+industry_rating character varying,
+quantity decimal,
+market_fair_value decimal,
+nav decimal 
+)
